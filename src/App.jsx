af128dadeca80cce90b663e5ad09a35606b1779d
DarkMGarner <markgarner337@gmail.com>
2022-12-18 00:53:43 -0400

    Create files

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import styles from "./style";
+
+const App = () => {
+  return (
+    <article className="bg-primary w-full overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Nav />
+        </div>
+      </div>
+
+      <section className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Hero />
+        </div>
+      </section>
+
+      <section className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Stats />
+          <Business />
+          <Billing />
+          <CardDeal />
+          <Testimonials />
+          <Clients />
+          <CTA />
+          <Footer />
+        </div>
+      </section>
+    </article>
+  );
+};
+export default App;
