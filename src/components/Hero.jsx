18616a7df30ef594a3fcf7fde9cf9e4cbc8f2530
DarkMGarner <markgarner337@gmail.com>
2022-12-18 02:10:18 -0400

    Add partial Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,26 @@
 import React from "react";
-
+import styles from "../style";
+import { discount, robot } from "../assets/static";
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section
+      id="home"
+      className={`flex md:flex-row flex-col ${styles.paddingY}`}
+    >
+      <div
+        className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}
+      >
+        <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
+          <img src={discount} alt="discount" className="w-[32px] h-[32px]" />
+          <p className={`${styles.paragraph} ml-2`}>
+            <span className="text-white">20%&nbsp;</span>
+            Discount For&nbsp;<span className="text-white">&nbsp;1 Month</span>
+            &nbsp;Account
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
