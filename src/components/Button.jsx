323cc5e87a3194969fcfefce6df3688dbf4db19f
DarkMGarner <markgarner337@gmail.com>
2022-12-22 00:34:55 -0400

    Complete Button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import styles from "../style";
 
-const Button = () => {
-  return <div>Button</div>;
+const Button = (props) => {
+  return (
+    <a
+      href="#getstarted"
+      className={`block py-4 px-6 bg-blue-gradient font-poppins font-medium text-lg text-primary outline-none ${props.styles}`}
+    >
+      Get Started
+    </a>
+  );
 };
 
 export default Button;
