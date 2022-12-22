e28affd6363dc0e6e608fc092fb7c3aedf16651e
DarkMGarner <markgarner337@gmail.com>
2022-12-22 15:15:51 -0400

    Complete Billing component

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -1,7 +1,48 @@
-import React from "react";
-
+import { apple, bill, google } from "../assets/static";
+import styles, { layout } from "../style";
 const Billing = () => {
-  return <div>Billing</div>;
+  return (
+    <section id="product" className={layout.sectionReverse}>
+      <div className={layout.sectionImgReverse}>
+        <img
+          src={bill}
+          alt="Billing"
+          className="w-full aspect-square relative z-50"
+        />
+
+        <div className="absolute z-30 -left-1/2 top-0 w-[50%] aspect-square rounded-full white__gradient"></div>
+        <div className="absolute z-0 -left-1/2 bottom-0 w-[50%] aspect-square rounded-full pink__gradient"></div>
+      </div>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>
+          Easily control your <br className="sm:block hidden" /> billing &
+          invoicing.
+        </h2>
+        <p className={`${styles.paragraph} max-w-[29.375rem] mt-5`}>
+          Elit enim sed massa etiam. Mauris eu adispicing ultrices ametodio
+          oenean neque. Fusce ipsum orci rhoncus aliporttitor integer platea
+          placerat.
+        </p>
+
+        <div className="flex flex-row flex-wrap sm:mt-10 mt-6">
+          <a href="#go_appstore" className="block">
+            <img
+              src={apple}
+              alt="AppStore"
+              className="w-32 h-[2.625rem] object-contain mr-5 cursor-pointer"
+            />
+          </a>
+          <a href="#go_playstore" className="block">
+            <img
+              src={google}
+              alt="PlayStore"
+              className="w-32 h-[2.625rem] object-contain mr-5 cursor-pointer"
+            />
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Billing;
