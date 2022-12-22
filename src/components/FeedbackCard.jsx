8e0d54ba3afa680ed5b635155a54cd09d2440339
DarkMGarner <markgarner337@gmail.com>
2022-12-22 19:52:46 -0400

    Complete FeedbackCard component

--- src/components/FeedbackCard.jsx
+++ src/components/FeedbackCard.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import { quotes } from "../assets/static";
+const FeedbackCard = ({ content, name, title, img }) => (
+  <div className="flex justify-between flex-col px-10 py-12 rounded-[1.125rem] max-w-[23.125rem] md:mr-10 sm:mr-5 mr-0 my-5 feedback-card">
+    <img
+      src={quotes}
+      alt="Double quotes"
+      className="w-[2.625rem] h-[1.6875rem] object-contain"
+    />
+    <p className="font-poppins font-normal text-lg leading-8 text-white my-10">
+      {content}
+    </p>
 
-const FeedbackCard = () => {
-  return <div>FeedbackCard</div>;
-};
+    <div className="flex flex-row">
+      <img src={img} alt={name} className="w-12 h-12 rounded-full" />
+      <div className="flex flex-col ml-4">
+        <h4 className="font-poppins font-semibold text-xl leading-8 text-white">
+          {name}
+        </h4>
+        <p className="font-poppins font-normal text-base leading-5 text-dimWhite">
+          {title}
+        </p>
+      </div>
+    </div>
+  </div>
+);
 
 export default FeedbackCard;
