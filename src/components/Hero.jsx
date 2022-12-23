2741072a7283fa6b5e0706676847abb65f22d110
DarkMGarner <markgarner337@gmail.com>
2022-12-22 22:40:01 -0400

    Revert last commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
   return (
     <section
       id="home"
-      className={`flex md:flex-row flex-col ${styles.paddingY} overflow-x-hidden`}
+      className={`flex md:flex-row flex-col ${styles.paddingY}`}
     >
       <div
         className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}
