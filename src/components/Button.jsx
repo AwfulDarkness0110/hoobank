789a5f169fa400b14169528ba0adb6adc679eaf7
DarkMGarner <markgarner337@gmail.com>
2022-12-22 21:07:55 -0400

    Reduce rounded-border size

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ const Button = (props) => {
   return (
     <a
       href="#getstarted"
-      className={`block py-4 px-6 bg-blue-gradient font-poppins font-medium text-lg text-primary outline-none ${props.styles} rounded-2xl cta`}
+      className={`block py-4 px-6 bg-blue-gradient font-poppins font-medium text-lg text-primary outline-none ${props.styles} rounded-xl cta`}
     >
       Get Started
     </a>
